3369c60304cc33eda0d9a7c6311cd6484e5a3142
WalmerHerrera <67081557+WalmerHerrera@users.noreply.github.com>
2021-10-12 18:11:42 -0500

    Add files via upload

---
+++ dashboard/vistas/parte_inferior.php
@@ -0,0 +1,77 @@
+
+      </div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>&copy; Realizado por @Calle, @Herrera, @Pisfil</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">¿Confirma salir y cerrar Sesión?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body"></div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
+          <a class="btn btn-primary" href="../MODELOS/Cerrar.php">Salir</a>
+  
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+ 
+
+  
+    <!-- datatables JS -->
+    <script type="text/javascript" src="vendor/datatables/datatables.min.js"></script>    
+    <!-- código propio JS --> 
+    <script type="text/javascript" src="codigosJS/mainVehiculo.js"></script>
+    <script type="text/javascript" src="codigosJS/main.js"></script> 
+    <script type="text/javascript" src="codigosJS/mainUsuario.js"></script> 
+    <script type="text/javascript" src="codigosJS/mainMarca.js"></script> 
+    <script type="text/javascript" src="codigosJS/mainModelo.js"></script>
+    <script type="text/javascript" src="codigosJS/mainTipoV.js"></script>
+    <script type="text/javascript" src="codigosJS/mainAgencia.js"></script>
+    <script type="text/javascript" src="codigosJS/mainReserva.js"></script>
+    <script type="text/javascript" src="codigosJS/mainAlquiler.js"></script>
+    <script type="text/javascript" src="codigosJS/mainDevolucion.js"></script>
+    <script type="text/javascript" src="codigosJS/mainPago.js"></script>
+    
+    
+</body>
+
+</html>
